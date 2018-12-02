70c9fb1a08a11a343fd544f3c92043c9845816d6
Kjplay <kuba10j@gmail.com>
2018-12-02 13:44:28 +0100

    Update index.js

--- tg/index.js
+++ tg/index.js
@@ -6,7 +6,7 @@ var fs = require("fs");
 var tongueTwister = {};
 //Get random number
 tongueTwister.random = function(min, max) {
-	min = typeof(min) == 'number' && min % 1 === 0 ? min : 0;
+    min = typeof(min) == 'number' && min % 1 === 0 ? min : 0;
     max = typeof(max) == 'number' && max % 1 === 0 ? max : 0;
     return Math.floor(Math.random()*(max-min+1)+min);
 };
